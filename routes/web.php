3f484ef220eed32a7acb853ca62ad9764ced6525
1noseA <1noseA2L@gmail.com>
2020-07-04 13:00:56 +0900

    [7-4]authを追加

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello','HelloController@index');
+Route::get('hello','HelloController@index')
+->middleware('auth');
 Route::post('hello','HelloController@post');
 
 Route::get('hello/add','HelloController@add');
@@ -57,3 +58,6 @@ Route::get('hello/rest', 'HelloController@rest');
 
 Route::get('hello/session','HelloController@ses_get');
 Route::post('hello/session','HelloController@ses_put');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
