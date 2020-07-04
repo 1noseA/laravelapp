5b1572bab5b2c706e9aca5de54722e28adf52cdc
1noseA <1noseA2L@gmail.com>
2020-07-04 14:40:13 +0900

    [7-5]最後までやったがテストが成功しない

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use App\Person;
 
 class HelloTest extends TestCase
 {
@@ -18,19 +19,28 @@ class HelloTest extends TestCase
 
     public function testHello()
     {
-        $this->assertTrue(true);
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        factory(User::class, 10)->create();
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
 
-        $response = $this->get('/');
-        $response->assertStatus(200);
-
-        $response = $this->get('/hello');
-        $response->assertStatus(302);
-
-        $user = factory(User::class)->create();
-        $response = $this->actingAs($user)->get('/hello');
-        $response->assertStatus(200);
-
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
+        factory(Person::class)->create([
+            'name' => 'XXX',
+            'email' => 'YYY@ZZZ.COM',
+            'password' => '123',
+        ]);
+        factory(Person::class, 10)->create();
+        $this->assertDatabaseHas('people', [
+            'name' => 'XXX',
+            'email' => 'YYY@ZZZ.COM',
+            'password' => '123',
+        ]);
     }
 }
